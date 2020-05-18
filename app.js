dd67f46f995d897bc6fddbd6da83f2ed6ee818fa
Emily Carter <ecarter21@gmail.com>
2020-05-18 14:04:08 -0500

    functions

---
+++ app.js
@@ -0,0 +1,24 @@
+let name = "Emily Carter";
+const states = 50;
+let result = (5 + 4);
+console.log(result);
+// 9 is input into the console  
+
+function sayHello() {
+    alert('Hello World!');
+
+}
+// displays pop up window alert
+
+sayHello()
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert('Sorry ' + name + ", you aren't old enough to view this page!");
+    }
+}
+checkAge('Charles', 21)
+checkAge('James', 18)
+checkAge('Abby', 27)
+checkAge('John', 17)
+
